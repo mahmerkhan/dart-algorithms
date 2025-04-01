f49ba0d883c13ee76c46239decde2258ffa43091
Muhammad Ahmer Khan <70189037+mahmerkhan@users.noreply.github.com>
2025-04-01 16:34:18 +0500

    Program to check palindrom

---
+++ check_palindrom.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  print("Please Enter a number to check whether its palindrom or not?");
+  var number = stdin.readLineSync(encoding: utf8);
+  bool result = isPalindrom(number ?? "");
+  print("The input string is : ${result}");
+}
+
+bool isPalindrom(String num) {
+  var original = num.toString();
+  var reversed = original.split('').reversed.join('');
+  return original == reversed;
+}
